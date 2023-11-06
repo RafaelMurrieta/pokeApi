eeae8c8523a81a86ca362175dcb1f7a3405f8f51
Rafel <r7121005846@gmail.com>
2023-11-06 13:15:58 -0600

    SE agrega el modal

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
 const container__card = document.getElementById('container-card')
+var __modal__ = document.getElementById('modal-pokemon')
 
-
-function cards(number,response) {
+function cards(number, response) {
     var card = document.createElement("div");
     card.id = number;
     card.className = "card";
     type = response['types']['0']['type']['name'];
-    Image = response['sprites']['other']['dream_world']['front_default']
-    height = response['height']
-    weight = response['weight']
-    //obtene el color
-    fetch(response.species.url)
-        .then(res => res.json())
-        .then(speciesData => {
-            const colorCard = speciesData.color.name;
-        });
-    card.innerHTML = '<div class="body-card"><img src='+Image+' alt='+response['name']+'> <h2>Nombre: '+response['name']+'</h2><p>Tipo de Pokemon: '+type+' </p><p>Altura: '+height+' m</p><p>Peso: '+weight+' kg</p><button type="submit">Ver mais</button></div>'
+    image = response['sprites']['other']['dream_world']['front_default'];
+    height = response['height'];
+    weight = response['weight'];
+    namePo = response['name'];
+    var cardContent = '<div class="body-card"><div class="imagen"><img src='+image+' alt='+namePo+'></div><h2>Nombre: '+namePo+'</h2><p>Tipo de Pokemon: '+type+' </p><p>Altura: '+height+' m</p><p>Peso: '+weight+' kg</p><div class="body-buton"><button class="openModal">Ver mas</button></div></div>';
+    card.innerHTML = cardContent;
     container__card.appendChild(card);
-    var stylesCard = document.getElementById(number)
-    console.log(stylesCard);
-    stylesCard.style.background = colorCard;
+    var boton = card.querySelector('.openModal');
+    boton.addEventListener('click', function() {
+        __modal__.style.display = "flex"
+    });
 }
 
-for (let number = 1; number <= 6; number++) {
+for (let number = 1; number <= 12; number++) {
     fetch('https://pokeapi.co/api/v2/pokemon/'+number)
         .then(res=>res.json())
-         .then(response=>{
-            cards(number, response)
-        })
-
+        .then(response=>{
+            cards(number, response);
+        });
 }
-
